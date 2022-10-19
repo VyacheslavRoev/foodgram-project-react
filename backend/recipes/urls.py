6da3a3b93c0635233cc6049e57a54c495c7d0a96
Вячеслав Роев <roev96@yandex.ru>
2022-10-19 16:33:55 +0300

    fixed urls2

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,10 +1,16 @@
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewset, RecipeViewSet, TagViewSet
+
+app_name = 'recipes'
 
 router = DefaultRouter()
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'ingredients', IngredientViewset, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 
-app_name = 'recipes'
+urlpatterns = [
+    path('', include(router.urls))
+]
+
