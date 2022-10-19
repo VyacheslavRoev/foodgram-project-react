f59c4f8c851157da0c33117683cf03b2a4e60ebe
Вячеслав Роев <roev96@yandex.ru>
2022-10-19 16:43:17 +0300

    fixed urls3

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views import IngredientViewset, RecipeViewSet, TagViewSet
 
-app_name = 'recipes'
-
 router = DefaultRouter()
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'ingredients', IngredientViewset, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
+
+app_name = 'recipes'
 
 urlpatterns = [
     path('', include(router.urls))
