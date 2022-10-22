d23a1bd740d613412406cc5c2d5cc4fdea2653ce
Вячеслав Роев <roev96@yandex.ru>
2022-10-22 23:08:14 +0300

    test shop12

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,6 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FONTS_ROOT = os.path.join(BASE_DIR, 'fonts/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,13 +1,18 @@
 import io
+import os
 
+from django.conf import settings
 from django.http import FileResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from reportlab.lib import pagesizes
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import cm
+from reportlab.lib.units import cm, inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.rl_config import defaultPageSize
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
@@ -101,12 +106,67 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
+    # @action(
+    #     detail=False,
+    #     methods=['GET'],
+    #     permission_classes=[IsAuthenticated]
+    # )
+    # def download_shopping_cart(self, request):
+    #     shopping_cart = ShoppingCart.objects.filter(user=request.user).all()
+    #     shopping_list = {}
+    #     for item in shopping_cart:
+    #         for ingredient_amount in item.recipe.ingredient_amount.all():
+    #             name = ingredient_amount.ingredient.name
+    #             unit = ingredient_amount.ingredient.measurement_unit
+    #             amount = ingredient_amount.amount
+    #             if name not in shopping_list:
+    #                 shopping_list[name] = {
+    #                     'name': name,
+    #                     'measurement_unit': unit,
+    #                     'amount': amount
+    #                 }
+    #             else:
+    #                 shopping_list[name]['amount'] += amount
+    #     content = (
+    #         [f'{item["name"]} ({item["measurement_unit"]}) '
+    #          f' - {item["amount"]}\n'
+    #          for item in shopping_list.values()]
+    #     )      
+    #     buffer = io.BytesIO()
+    #     p  = canvas.Canvas (buffer, pagesize=A4, bottomup=0)
+    #     textob  = p.beginText()
+    #     textob.setTextOrigin(cm, cm)
+    #     textob.setFont("Helvetica", 14)
+    #     for line in content:
+    #         textob.textLine(line)
+    #     p.drawText(textob)
+    #     p.showPage()
+    #     p.save()
+    #     buffer.seek(0)
+    #     return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
     @action(
         detail=False,
-        methods=['GET'],
-        permission_classes=[IsAuthenticated]
-    )
+        permission_classes=(IsAuthenticated,)
+        )
     def download_shopping_cart(self, request):
+        def firstPageContent(page_canvas, document):
+            header_content = 'Список покупок'
+            headerHeight = defaultPageSize[1] - 50
+            headerWidth = defaultPageSize[0]/2.0
+
+            page_canvas.saveState()
+            page_canvas.setFont('Verdana', 18)
+            page_canvas.drawCentredString(
+                headerWidth,
+                headerHeight,
+                header_content
+            )
+            page_canvas.restoreState()
+        
+        pdfmetrics.registerFont(
+            TTFont('Verdana', os.path.join(settings.FONTS_ROOT, 'Verdana.ttf'))
+        )
+
         shopping_cart = ShoppingCart.objects.filter(user=request.user).all()
         shopping_list = {}
         for item in shopping_cart:
@@ -126,18 +186,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
             [f'{item["name"]} ({item["measurement_unit"]}) '
              f' - {item["amount"]}\n'
              for item in shopping_list.values()]
-        )      
+        )
+
         buffer = io.BytesIO()
-        p  = canvas.Canvas (buffer, pagesize=A4, bottomup=0)
-        textob  = p.beginText()
-        textob.setTextOrigin(cm, cm)
-        textob.setFont("Helvetica", 14)
-        for line in content:
-            textob.textLine(line)
-        p.drawText(textob)
-        p.showPage()
-        p.save()
+        document = SimpleDocTemplate(
+            buffer,
+            pagesize=pagesizes.portrait(pagesizes.A4),
+        )
+
+        columns_width = [6*inch, 1*inch, 1*inch]
+        table = Table(
+            content,
+            rowHeights=20,
+            repeatRows=1,
+            colWidths=columns_width,
+            hAlign='CENTER'
+        )
+
+        table.setStyle(TableStyle([
+            ('FONTSIZE', (0, 0), (-1, -1), 18),
+            ('FONTNAME', (0, 0), (-1, -1), "Verdana"),
+        ]))
+
+        document.build([table], onFirstPage=firstPageContent)
+
         buffer.seek(0)
-        return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
+        return FileResponse(
+            buffer, as_attachment=True, filename='shopping_cart.pdf',
+        )
         
         
