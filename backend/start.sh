6dda1d04f130eb29f62157477d8bfbd3f490a2c4
Вячеслав Роев <roev96@yandex.ru>
2022-10-23 10:40:30 +0300

    test docker5

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gunicorn --bind :8000 dockerdjango.wsgi:application
+gunicorn --bind :8000 --workers=5 --threads=2 dockerdjango.wsgi:application
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
