211d1e5b67761348421920f1ec3c6472d07b9db6
Вячеслав Роев <roev96@yandex.ru>
2022-10-24 15:12:07 +0300

    test subscriptions13

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from recipes.pagination import RecipePaginator
+from users.models import User
+from .models import Subscription
+from .serializers import SubscriptionSerializer
+
+
+class SubscribeView(APIView):
+    """Добавление и удаление подписки."""
+    serializer_class = SubscriptionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        author = get_object_or_404(User, id=user_id)
+        Subscription.objects.create(
+            user=request.user,
+            author_id=user_id
+        )
+        return Response(
+            self.serializer_class(author, context={'request': request}).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    def delete(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        subscription = Subscription.objects.filter(
+            user=request.user,
+            author_id=user_id
+        )
+        if subscription:
+            subscription.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SubscribeListView(ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = SubscriptionSerializer
+    pagination_class = RecipePaginator
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(following__user=user)
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,6 @@
 from djoser.views import UserViewSet
-from rest_framework import status
-from rest_framework.generics import ListAPIView, get_object_or_404
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from recipes.pagination import RecipePaginator
-from subscriptions.models import Subscription
-from subscriptions.serializers import SubscriptionSerializer
 from .models import User
 from .serializers import UserSerializer
 
@@ -17,42 +9,3 @@ class CustomUserViewset(UserViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
-
-
-class SubscribeView(APIView):
-    """Добавление и удаление подписки."""
-    serializer_class = SubscriptionSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request, *args, **kwargs):
-        user_id = self.kwargs.get('user_id')
-        author = get_object_or_404(User, id=user_id)
-        Subscription.objects.create(
-            user=request.user,
-            author_id=user_id
-        )
-        return Response(
-            self.serializer_class(author, context={'request': request}).data,
-            status=status.HTTP_201_CREATED
-        )
-
-    def delete(self, request, *args, **kwargs):
-        user_id = self.kwargs.get('user_id')
-        subscription = Subscription.objects.filter(
-            user=request.user,
-            author_id=user_id
-        )
-        if subscription:
-            subscription.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class SubscribeListView(ListAPIView):
-    queryset = User.objects.all()
-    permission_classes = (IsAuthenticated,)
-    serializer_class = SubscriptionSerializer
-    pagination_class = RecipePaginator
-
-    def get_queryset(self):
-        user = self.request.user
-        return User.objects.filter(following__user=user)
