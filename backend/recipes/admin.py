4f94f42ca1a12a9eaf899ad6361f83c293095595
Вячеслав Роев <roev96@yandex.ru>
2022-10-27 11:59:59 +0300

    test admin 4

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -42,6 +42,32 @@ class UsersAdmin(admin.ModelAdmin):
         ShoppingInline
     ]
     empty_value_display = "-пусто-"
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        if not is_superuser:
+            disabled_fields |= {
+                'is_superuser',
+                'user_permissions',
+            }
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
 
 
 class TagAdmin(admin.ModelAdmin):
