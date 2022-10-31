ff7d2624ca261ade6d8f95282441f39dbf80c663
Вячеслав Роев <roev96@yandex.ru>
2022-10-31 11:05:56 +0300

    text lang 8

--- backend/recipes/migrations/0004_auto_20221028_1123.py
+++ backend/recipes/migrations/0004_auto_20221028_1123.py
@@ -19,3 +19,14 @@ class Migration(migrations.Migration):
             options={'verbose_name': 'Тэг в рецепте', 'verbose_name_plural': 'Тэги в рецепте'},
         ),
     ]
+    
+    dependencies = [
+        ('authtoken', '0003_tokenproxy'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='tokenproxy',
+            options={'verbose_name': 'Токен', 'verbose_name_plural': 'Токены'},
+        ),
+    ]
